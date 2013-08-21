69d2f5f347a3298b9ca400478632c3f1c5b2f149
Rakesh Menon <menonrakesh@live.com>
2013-08-21 14:01:29 +0530

    re-structured

---
+++ config/express.js
@@ -0,0 +1,13 @@
+module.exports = function (app, express, passport) {
+  app.use(express.static(__dirname + '/public'));
+  app.use(express.bodyParser());
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: 'some random shit'
+  }));
+  app.use(passport.initialize());
+  app.use(passport.session());
+
+  app.set('views', './views');
+  app.set('view engine', 'ejs');
+};
