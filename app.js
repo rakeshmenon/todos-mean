47738334945404d421cbf75f18252ef977b53af2
Rakesh Menon <menonrakesh@live.com>
2013-08-30 23:29:31 +0530

    init setup

--- app.js
+++ app.js
@@ -1,35 +1,11 @@
 var express = require('express');
-var mode = "dev";
-
-//passport vars
-var passport = require('passport'),
-    ensureLoggedIn = require('connect-ensure-login').ensureLoggedIn,
-    loginStrategies = {};
-
-// OAuth login strategies - Add twitter and google
-loginStrategies.FacebookStrategy = require('passport-facebook').Strategy;
 
 //express app
 var app = express();
 
-//mongoose vars
-var ObjectId = require('mongoose').Types.ObjectId;
 
-//switch to production mode if NODE_ENV is set to "production"
-app.configure("production", function () {
-  mode = "production";
-});
-
-/**
- * App configurations
- */
-var Models = require("./config/models")(mode || "dev");
-
-require("./config/passport")(passport, loginStrategies, Models, mode || "dev");
-
-require("./config/express")(app, express, passport, mode || "dev");
-
-require("./config/routes")(app, passport, ensureLoggedIn, Models, mode || "dev");
+require("./config/express")(app, express);
+require("./config/routes")(app);
 
 
 var port = process.env.PORT || 5000; //for heroku
